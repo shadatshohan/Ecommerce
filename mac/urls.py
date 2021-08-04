1cd0ae7d09edb0128ca2d99bca4188f83026c097
shohan <shadatshohan2@gmail.com>
2021-08-04 12:17:35 +0600

    urlsdlt

--- mac/urls.py
+++ mac/urls.py
@@ -25,6 +25,4 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('blog/',include('blog.urls')),
     path('shop/',include('shop.urls')),
-     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
